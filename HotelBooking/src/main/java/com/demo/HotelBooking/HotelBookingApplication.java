37ae0b139c573e3b68ac2eca4114c47c9ae731bf
Adithya Lakshmi <adithya0427@gmail.com>
2022-01-30 20:17:44 +0530

    committing main class and pom.xml

---
+++ HotelBooking/src/main/java/com/demo/HotelBooking/HotelBookingApplication.java
@@ -0,0 +1,13 @@
+package com.demo.HotelBooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelBookingApplication.class, args);
+	}
+
+}
